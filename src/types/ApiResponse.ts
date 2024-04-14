f3247836c5ceef711e4b62cfd50331a251911f20
cmrohityadav <cmrohityadav23@gmail.com>
2024-04-14 20:16:25 +0530

    signup api

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,6 +1,6 @@
 import { Message } from "@/model/User";
 export interface ApiResponse{
-    succss:boolean,
+    success:boolean,
     message:string,
     isAcceptingMessage?:boolean,
     messages?:Array<Message>,
